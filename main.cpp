5704d5b9e1615feb8668bf3dac66c5da6e557a23
M.Ghanem <mohamed.ghanem@nabtahealth.com>
2019-12-29 12:10:30 +0200

    User can choose to generate a random test

--- main.cpp
+++ main.cpp
@@ -7,15 +7,30 @@
 
 using namespace std;
 
+#define TEST "test.txt"
+
 int main()
 {
-    
-    cout << "Insert your equations, bruh \n";
-    //freopen("13-equations.txt", "r", stdin);
+    char c;
+    do
+    {
+        cout << "Generate a random test? [Y]es/[N]o: ";
+        cin >> c;
+    } while (c != 'Y' && c != 'N');
+
+    if(c == 'Y')
+    {
+        int n;
+        cout << "Insert number of equations: ";
+        cin >> n;
+        writeRandomTest(n, TEST);
+    }
+
+    cout << "Equations are taken from the test.txt file \n";
     ifstream in("test.txt");
     string eq;
     vector<Equation> Eqs;
-    while(getline(in, eq) && !eq.empty())
+    while (getline(in, eq) && !eq.empty())
     {
         Eqs.push_back(Equation(eq));
     }
@@ -25,17 +40,16 @@ int main()
     map<string, double> sol = system.solve();
     chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
-    for(auto it: sol)
+    for (auto it : sol)
     {
         cout << it.first << " = " << it.second << endl;
     }
     cout << "==================================\n";
-    cout << "Time taken = " << chrono::duration_cast<std::chrono::microseconds>(end - begin).count()/1000.0 << " milliseconds" << std::endl;
+    cout << "Time taken = " << chrono::duration_cast<std::chrono::microseconds>(end - begin).count() / 1000.0 << " milliseconds" << std::endl;
     cout << "==================================\n";
-    
-   /*
+
+    /*
    int n;
    cin >> n;
    writeRandomTest(n, "test.txt");*/
-   
 }
